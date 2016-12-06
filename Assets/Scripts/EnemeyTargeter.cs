48fe5d98b2f7d45ce319675f42d58fb7bf352510
Roman Frołow <rofrol@gmail.com>
2016-12-06 18:52:58 +0100

    first commit

---
+++ Assets/Scripts/EnemeyTargeter.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemeyTargeter : MonoBehaviour
+{
+
+    List<Enemy> enemiesInRange = new List<Enemy>();
+
+    void Update()
+    {
+        Enemy closestEnemy = null;
+        if (enemiesInRange.Count > 0)
+            closestEnemy = enemiesInRange[0];
+
+        foreach(Enemy enemy in enemiesInRange)
+        {
+            if(Vector3.Distance(this.transform.position, closestEnemy.transform.position) >
+                Vector3.Distance(this.transform.position, enemy.transform.position)) {
+                closestEnemy = enemy;
+            }
+        }
+
+        if (closestEnemy != null)
+        {
+            Debug.LogError(closestEnemy.name);
+            this.transform.LookAt(closestEnemy.transform.position);
+        }
+            
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        Enemy enemy = col.gameObject.GetComponent<Enemy>();
+        Debug.LogError(col.gameObject.name);
+
+        if (enemy != null && !enemiesInRange.Contains(enemy))
+        {
+            enemiesInRange.Add(enemy);
+        }
+        else
+            Debug.LogError(enemy);
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        Enemy enemy = col.gameObject.GetComponent<Enemy>();
+        Debug.LogError(col.gameObject.name);
+
+        if (enemy != null && enemiesInRange.Contains(enemy))
+        {
+            enemiesInRange.Remove(enemy);
+        }
+        else
+            Debug.LogError(enemy);
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+2016-12-06 Coroutines
+
+dzialo, dodawane jest do enemiesInRange jak Enemy wejdzie w kolizje z TowerCube.
+
+https://docs.unity3d.com/ScriptReference/MonoBehaviour.StartCoroutine.html
